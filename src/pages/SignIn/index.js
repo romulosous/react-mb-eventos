6b146d3c38f47c22c1a9a6a6e21242faae9b9790
Romulo Carvalho <romulocarvalho081@gmail.com>
2021-11-01 16:08:44 -0300

    login screen

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import { Link } from 'react-router-dom'
+import "./signin.css"
+import logo from "../../assets/lateral-image.jpg"
+
 
 function SignIn() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert("CLICOU")
+  }
+
   return (
-    <div>
-      <h1>PAGINA DE LOGIN</h1>
+    <div className="container">
+      <div className="login">
+        <form onSubmit={handleSubmit}>
+          <Link to="/">MB Events</Link>
+          <h1>Log in</h1>
+          <input type="text" placeholder="email@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="password" placeholder="**********" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button type="submit">Acessar</button>
+        </form>
+
+        <Link to="/register">Criar uma conta</Link>
+      </div>
+
+      <div className="login-area">
+      </div>
     </div>
   );
 }
