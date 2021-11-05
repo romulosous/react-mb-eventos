971c69199456a04ba89bbf4f1c4476f172b56a43
Romulo Carvalho <romulocarvalho081@gmail.com>
2021-11-05 10:32:56 -0300

    add SideBar

---
+++ src/components/SideBar/index.js
@@ -0,0 +1,29 @@
+import { useContext } from "react"
+import "./sidebar.css"
+import { AuthContext } from "../../contexts/auth"
+import avatar from "../../assets/avatar.png"
+
+import { Link } from "react-router-dom"
+
+export default function SideBar() {
+  const { user } = useContext(AuthContext)
+  return (
+    <div className="sidebar">
+      <div>
+        <img width="80" src={user.avatarUrl === null ? avatar : user.avatarUrl} />
+      </div>
+
+      <Link to="/events">
+        Meus Eventos
+      </Link>
+      <Link to="/registrations">
+        Eventos Inscritos
+      </Link>
+
+      <Link to="/user">
+        Editar Perfil
+      </Link>
+
+    </div>
+  )
+}
