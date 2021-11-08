ffacb5d066cc4d739e9a9aa3d35ff2abff99507f
Romulo Carvalho <romulocarvalho081@gmail.com>
2021-11-07 22:36:34 -0300

    creating list of events in my events

--- src/pages/UserEvents/index.js
+++ src/pages/UserEvents/index.js
@@ -3,11 +3,11 @@ import "./events.css"
 import Title from "../../components/Title"
 import SideBar from "../../components/SideBar"
 // import Header from "../../components/Header"
-import { FiPlus } from "react-icons/fi"
+import { FiPlus, FiMoreVertical } from "react-icons/fi"
 import { Link } from "react-router-dom"
 
 export default function UserEvents() {
-  const [events, setEvents] = useState([])
+  const [events, setEvents] = useState([1])
   return (
     <div>
       <SideBar />
@@ -31,6 +31,29 @@ export default function UserEvents() {
               <FiPlus size={25} color="#FFF" />
               Novo evento
             </Link>
+
+            <table>
+              <thead>
+                <th scope="col">Evento</th>
+                <th scope="col">Vendidos</th>
+                <th scope="col">Bruto</th>
+                <th scope="col">Estado</th>
+                <th scope="col">#</th>
+              </thead>
+              <tbody>
+                <tr>
+                  <td data-label="Evento">Event GO</td>
+                  <td data-label="Vendido"> 0/ 20 </td>
+                  <td data-label="Bruto">R$0,00</td>
+                  <td data-label="Estado"><span className="badge" style={{ backgroundColor: '#5cb85c' }} >À venda</span></td>
+                  <td data-label="#">
+                    <button className="action" style={{ background: 'none' }}>
+                      <FiMoreVertical />
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </>
         )}
 
